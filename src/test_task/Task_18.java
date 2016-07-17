326262a913daf0469b39c918e89d97fbb987d200
Konstantin <kos-nov07@mail.ru>
2016-07-17 11:44:21 +0300

    Test_task_17

---
+++ src/test_task/Task_18.java
@@ -0,0 +1,22 @@
+package test_task;
+
+/**
+ * Created by Константин on 14.07.2016.
+ * Given the following LOCs
+ */
+public class Task_18 {
+
+   public static void main (String [] args) {
+       int rate = 10;
+       //XXX amount = 1 - rate/100*1 - rate/100;
+       // System.out.println(amount);
+       /** What can XXX be?
+        * a) only int or long
+        * b) only long or double
+        * c) only double
+        * d) double or float
+        * e) long or double but not int or float
+        * f) int, long, float or double
+        */
+    }
+}
